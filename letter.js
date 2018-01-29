6bd912453ec0071e4f9e9ecdf0e03ca36e033dc5
eggobiggie <miacrandell@gmail.com>
2018-01-29 15:02:34 -0700

    set up constructor interaction for word and letter

--- letter.js
+++ letter.js
@@ -1,10 +1,33 @@
-var inquirer = require('inquirer');
-inquirer.prompt([
-    {
-        type: 'input',
-        message: "what's the word",
-        name: "word"
+const inquirer = require('inquirer');
+const Word = require("./Word");
+const fs = require("fs");
+let guessData;
+
+const Letter = function(guessesRemaining, letter) {
+    this.guessesRemaining = 10;
+    this.letter = letter;
+    this.newWordChoice = function(word) {
+        fs.readFile("log.txt", "utf8", function(error, data) {
+            if (error) {
+                console.log(error);
+            }
+            guessData = data;
+            console.log(guessData);
+        });    
     }
-]).then(answers => {
-    console.log(JSON.stringify(answers, null, ' '));
-});
+}
+
+let newLetter = new Letter();
+newLetter.newWordChoice();
+// newLetter.underscoreLetter();
+
+
+// inquirer.prompt([
+//     {
+//         type: 'input',
+//         message: "what's the word",
+//         name: "word"
+//     }
+// ]).then(answers => {
+//     console.log(JSON.stringify(answers, null, ' '));
+// });
--- word.js
+++ word.js
@@ -1,14 +1,19 @@
+const fs = require("fs");
+
 function Word(word) {
     this.word = word;
     this.getWord = function() {
         let wordArray = ["camembert", "wensleydale", "mozzarella", "provolone", "gorgonzola", "manchego", "gouda", "monterey jack", "havarti", "marscapone", "ricotta", "muenster", "roquefort", "asiago"];
-        let wordChoice = wordArray[Math.floor(Math.random() * wordArray.length)];
-        console.log(wordChoice);
+        this.word = wordArray[Math.floor(Math.random() * wordArray.length)];
+        fs.writeFile("log.txt", this.word, function(error) {
+            if (error) {
+                console.log(error);
+            }
+        });
     }
 }
 
 let newWord = new Word();
-
 newWord.getWord();
 
 module.exports = Word;
